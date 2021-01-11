d1b683db38d98af5a01540deee4e434004396133
hanhanhanz <69136662+hanhanhanz@users.noreply.github.com>
2021-01-11 00:52:54 -0500

    Add files via upload

--- urlgrepper.go
+++ urlgrepper.go
@@ -9,8 +9,13 @@ import "flag"
 import "bufio"
 import "net/url"
 import "strings"
-
+import "sync"
+import "net/http"
+import "crypto/tls"
+//import "errors"
+//import "net"
 //import "github.com/gijsbers/go-pcre"
+
 type conf struct {
 	Url string
 	Urls string
@@ -62,6 +67,198 @@ func errorKill(m string) {
 	
 }
 
+func urltoslice(url string, urls string) []string {
+	var seeds = []string{}
+	if url == "" && urls == "" {
+		errorKill("-u or -ul is mandatory")
+	} else if url != "" && urls != "" {
+		errorKill("choose either -u or -ul")
+	} else if url != "" {
+		_, err := regexp.Match(`^https?://`, []byte(url))
+		if err != nil {
+	    	fmt.Printf("[-] regex.match failed  : ")
+	    	panic(err)
+	    }
+		seeds = append(seeds,url)
+		
+	} else if urls != "" {
+		//prepare to read file from -ul
+		var g *os.File
+    	var g2 *bufio.Scanner
+    	g,_ = os.Open(urls) 
+	    g2 = bufio.NewScanner(g)
+
+    	for g2.Scan() {
+    		var line = g2.Text()
+    		_, err := regexp.Match(`^https?://`, []byte(url))
+			if err != nil {
+	    		fmt.Printf("[-] regex.match failed  : ")
+	    		panic(err)
+	    	}
+			seeds = append(seeds,line)
+			
+    	}
+	}
+	return seeds
+		
+}
+
+
+func myrequest(nc *retryablehttp.Client, seed string, wg *sync.WaitGroup)  (error,string) {
+	body2 := ""
+
+		req, err := retryablehttp.NewRequest("GET",seed, nil)
+		if err != nil {
+	    	wg.Done()
+			return err,body2
+	    }
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
+
+		//send request
+		resp, err := nc.Do(req)
+		
+		if err != nil {
+			wg.Done()
+			return err,body2
+		}
+		
+
+		//reading request
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+	    	wg.Done()
+			return err,body2
+	    }
+	   resp.Body.Close()
+	body2 = string(body)
+	wg.Done()
+	return err,body2
+}
+
+func urlprocess(body2 string, seed string, c conf, wg *sync.WaitGroup) ([]string) {
+	//normal URL regex
+	clean := []string{}
+	pat := regexp.MustCompile(`http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+`)	
+	raws := pat.FindAllString(body2,-1)
+	
+	//removing all param
+	for i, s := range raws {
+		temp := strings.Split(s,"?")
+		temp = strings.Split(temp[0],")")
+		temp = strings.Split(temp[0],"\"")
+		raws[i] = temp[0]
+		
+	}
+	
+	//migrate to url format for easy URL segmentation
+	unparse,err := url.QueryUnescape(seed)
+	if err != nil {
+		fmt.Printf("[-] QueryUnescape failed  : ")
+		panic(err)
+	}
+
+	//get domain
+	var parseed *(url.URL)
+	if c.Toxtract != "" {
+		parseed,err = url.Parse(c.Toxtract)
+		if err != nil {
+			fmt.Printf("[-] get domain url.Parse1 failed  : ")
+			panic(err)
+		}
+
+	} else {
+		parseed,err = url.Parse(unparse)
+		if err != nil {
+			fmt.Printf("[-] get domain url.Parse2 failed  : ")
+			panic(err)
+		}
+	}
+	
+	//process what to xtract
+	for _,raw := range raws {
+		//if mode domain
+		if c.Mode == "domain" {
+			//domain regex
+			unparse,err := url.QueryUnescape(raw)
+			if err != nil {
+    			fmt.Printf("[-] QueryUnescape failed  : ")
+    			panic(err)
+    		}
+			
+			u,err := url.Parse(unparse)
+			if err != nil {
+    			fmt.Printf("[-] process url.Parse failed  : ")
+    			panic(err)
+    		}
+
+			//parse to obtain domain/subdomain specified in -u/-ul
+			
+			tem := strings.ReplaceAll(parseed.Host,".","\\.") 	
+							
+			pat2 := regexp.MustCompile(fmt.Sprintf(`(^|^[^:]+:\/\/|[^\.]+\.)`+tem))
+			//pat2 := regexp.MustCompile(fmt.Sprintf(`(^|^[^:]+:\/\/|[^\.]+\.)w3\.com`))
+			temp := pat2.FindAllString(u.Host,-1)
+			if temp != nil {
+				clean = append(clean,temp[0])
+				
+				
+			}
+			
+		//if mode url
+		} else if c.Mode == "url" {
+			//mode xtennion enabled
+			if c.Xtension != "" {
+
+				formats := strings.Split(c.Xtension,",")
+				
+				formatx := ""
+				for i := 0; i < len(formats); i++ {
+				//for i, s := range formats {
+					
+					formatx += fmt.Sprintf("."+formats[i])
+					if i != len(formats) -1 {
+						formatx += "|"
+					}
+
+				}
+
+				re:= regexp.MustCompile(`([a-zA-Z0-9\s_\\.\-\(\):])+(`+formatx+`)$`)
+				if re.MatchString(raw) {
+					clean = append(clean,raw)				
+				}
+			//mode xtension disabled
+			} else {
+				clean = append(clean,raw)
+			}
+			
+		}	
+	}
+	wg.Done()
+	return clean
+		
+}
+
+func cleanandstore(c conf, clean []string, wg *sync.WaitGroup) {
+	clean = removeDuplicateValues(clean)
+	for _, s := range clean {
+		fmt.Println(s)
+		if c.Outname != "" {
+			storehere(s+"\n",c.Outfile)
+		}
+	}
+	wg.Done()
+}
+
+func cleanandstore2(c conf, clean []string) {
+	clean = removeDuplicateValues(clean)
+	for _, s := range clean {
+		fmt.Println(s)
+		if c.Outname != "" {
+			storehere(s+"\n",c.Outfile)
+		}
+	}
+}
+
 
 func main() {
 	
@@ -71,186 +268,133 @@ func main() {
 	flag.StringVar(&(c.Urls),"ul","","source where url will be taken in a file")
 	flag.StringVar(&(c.Mode),"m","domain","choose what to extract (domain,url)")
 	flag.StringVar(&(c.Outname),"o","","output result to a file")
-	flag.IntVar(&(c.Thread),"t",10,"goroutine number to be utilized (kinda like thread) (in development)")
+	flag.IntVar(&(c.Thread),"t",0,"goroutine number to be utilized (kinda like thread) (in development)")
 	flag.StringVar(&(c.Xtension),"x","","xtension to extract (only work for URL mode)")
-	flag.StringVar(&(c.Toxtract),"tx","","specify extact domain if it is different with source URL requested (in development)")
+	flag.StringVar(&(c.Toxtract),"tx","","specify extract domain if it is different with source URL requested (in development)")
 	flag.Parse()
 
-	//building client
+	///building client
+	//disable cert check
+	customTransport := http.DefaultTransport.(*http.Transport).Clone()
+	customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	customTransport.IdleConnTimeout = time.Second * 5
+	//default retry conf
 	var nc = retryablehttp.NewClient()
-	nc.RetryWaitMin = 1 * time.Second
+	nc.RetryWaitMin = 2 * time.Second
 	nc.RetryWaitMax = 2 * time.Second
-	nc.RetryMax = 3
+	nc.RetryMax = 2
 	nc.Logger = nil
+	nc.HTTPClient = &http.Client{Transport: customTransport}
 	
-
-	//body := bytes.NewReader([]byte(""))
+	
 	var seeds = []string{}
-	if c.Url == "" && c.Urls == "" {
-		errorKill("-u or -ul is mandatory")
-	} else if c.Url != "" && c.Urls != "" {
-		errorKill("choose either -u or -ul")
-	} else if c.Url != "" {
-		_, err := regexp.Match(`^https?://`, []byte(c.Url))
-		if err != nil {
-	    	fmt.Printf("[-] regex.match failed  : ")
-	    	panic(err)
-	    }
-		seeds = append(seeds,c.Url)
-	} else if c.Urls != "" {
-		//prepare to read file from -ul
-		var g *os.File
-    	var g2 *bufio.Scanner
-    	g,_ = os.Open(c.Urls) 
-	    g2 = bufio.NewScanner(g)
-
-    	for g2.Scan() {
-    		var line = g2.Text()
-    		_, err := regexp.Match(`^https?://`, []byte(c.Url))
-			if err != nil {
-	    		fmt.Printf("[-] regex.match failed  : ")
-	    		panic(err)
-	    	}
-			seeds = append(seeds,line)
-    	}
-	}
+	seeds = urltoslice(c.Url,c.Urls)
 	
 	//prepare file for -o
 	if c.Outname != "" {
-	     var _ = os.Remove(c.Outname)
+	    var _ = os.Remove(c.Outname)
 	    f,err := os.OpenFile(c.Outname,os.O_APPEND|os.O_WRONLY|os.O_CREATE,0644)
 	    if err != nil {
 	    	fmt.Printf("[-]create file error : ")
 	    	panic(err)
 	    }
 	    c.Outfile = f
-
     }
-
-	clean := []string{}
 	
+	
+ 	
+    
+    //final result slice declaration
+    clean := []string{}
+
+    //preparing thread
+    if c.Thread == 0 {
+		c.Thread = len(seeds) 
+	}
+
+	//channel declaration
+	body2 := make(chan string, len(seeds))
+	body3 := make(chan string, len(seeds))
+	body4 := make(chan []string, len(seeds))
+	
+	//goroutine guard number
+    guard1 := make(chan struct{}, c.Thread)
+    guard2 := make(chan struct{}, c.Thread)
+    guard3 := make(chan struct{}, c.Thread)
+
+    var wg sync.WaitGroup
+ 	wg.Add(len(seeds))
+//=======================================================================================================================================================================================================================
 	for _,seed := range seeds {
-
 		//building request
-		req, err := retryablehttp.NewRequest("GET",seed, nil)
-		if err != nil {
-	    	fmt.Printf("[-] new request failed : ")
-	    	panic(err)
-	    }
-		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
-
-		//send request
-		resp, err := nc.Do(req)
-		defer resp.Body.Close()
-		if err != nil {
-				fmt.Println("error")
-				os.Exit(3)
+		guard1 <- struct{}{}
+		go func(seed string)  {
+			
+			err,data := myrequest(nc,seed,&wg)
+			if err != nil {
+				if strings.Contains(err.Error(), "i/o timeout") {
+	    			data = ""
+	    		} else {
+	    			fmt.Printf("[-] myrequest func error : ")
+	    			fmt.Println(err)
+	    			data = ""
+	    		} 	
 			}
-		
-		//reading request
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-	    	fmt.Printf("[-] Reading request failed : ")
-	    	panic(err)
-	    }
-		body2 := string(body)
 
-		//normal URL regex
-		pat := regexp.MustCompile(`http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+`)
+			body2 <- data
+			body3 <- seed
 		
-		raws := pat.FindAllString(body2,-1)
+			<-guard1
+			
+		}(seed)
 		
-		//removing all param
-		for i, s := range raws {
-			temp := strings.Split(s,"?")
-			temp = strings.Split(temp[0],")")
-			temp = strings.Split(temp[0],"\"")
-			raws[i] = temp[0]
+	}
+//=======================================================================================================================================================================================================================
+
+	wg.Wait()
+	wg.Add(len(seeds))
+	for i := 0; i < len(seeds); i++ {
+		guard2 <- struct{}{}
+		go func() {
+
+			message1 := <-body2
+			message2 := <-body3
+			body4 <- urlprocess(message1,message2,c,&wg)
+			<-guard2
+		}()
+
+	}
+	
+	wg.Wait()
+	close(body2)
+	close(body3)
+	
+
+//=======================================================================================================================================================================================================================				
+	if c.Toxtract != "" {
+		for i := 0; i < len(seeds); i++ {
+	
+			message3 := <- body4
+			for _,mes := range message3 {
+				clean = append(clean,mes)
+			}
+		}
+		cleanandstore2(c,clean)
+	} else {
+		wg.Add(len(seeds))
+
+		for i := 0; i < len(seeds); i++ {
+			guard3 <- struct{}{}
+			go func() {
+				message3 := <- body4
+				cleanandstore(c,message3,&wg)
+				<-guard3
+			}()
 			
 		}
-		
-		
-		unparse,err := url.QueryUnescape(seed)
-		
-		
-		if err != nil {
-			fmt.Printf("[-] QueryUnescape failed  : ")
-			panic(err)
-		}
-
-		var parseed *(url.URL)
-		if c.Toxtract != "" {
-			parseed,err = url.Parse(c.Toxtract)
-		} else {
-			parseed,err = url.Parse(unparse)
-		}
-		
-
-		for _,raw := range raws {
-			//if mode domain
-			if c.Mode == "domain" {
-				//domain regex
-				unparse,err := url.QueryUnescape(raw)
-				if err != nil {
-	    			fmt.Printf("[-] QueryUnescape failed  : ")
-	    			panic(err)
-	    		}
-				u,err := url.Parse(unparse)
-
-				//parse to obtain domain/subdomain specified in -u/-ul
-				
-				tem := strings.ReplaceAll(parseed.Host,".","\\.") 	
-								
-				//pat2 := regexp.MustCompile(fmt.Sprintf(`(^|^[^:]+:\/\/|[^\.]+\.)`+tem))
-				pat2 := regexp.MustCompile(fmt.Sprintf(`(^|^[^:]+:\/\/|[^\.]+\.)w3\.com`))
-				temp := pat2.FindAllString(u.Host,-1)
-				if temp != nil {
-					clean = append(clean,temp[0])
-					
-					
-				}
-				
-
-			//if mode url
-			} else if c.Mode == "url" {
-				//mode xtennion enabled
-				if c.Xtension != "" {
-
-					formats := strings.Split(c.Xtension,",")
-					
-					formatx := ""
-					for i := 0; i < len(formats); i++ {
-					//for i, s := range formats {
-						
-						formatx += fmt.Sprintf("."+formats[i])
-						if i != len(formats) -1 {
-							formatx += "|"
-						}
-
-					}
-
-					//re:= regexp.MustCompile(`([a-zA-Z0-9\s_\\.\-\(\):])+(.js|.docx|.pdf)$`)
-					re:= regexp.MustCompile(`([a-zA-Z0-9\s_\\.\-\(\):])+(`+formatx+`)$`)
-					if re.MatchString(raw) {
-						clean = append(clean,raw)				
-					}
-				//mode xtension disabled
-				} else {
-					clean = append(clean,raw)
-				}
-				
-			}	
-		}
-		
+		wg.Wait()
+		close(body4)
 		
 	}
-	
-	clean = removeDuplicateValues(clean)
-	
-	for _, s := range clean {
-		fmt.Println(s)
-		if c.Outname != "" {
-			storehere(s+"\n",c.Outfile)
-		}
-	}
+
 }
