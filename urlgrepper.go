ef82cb6577cf8f349551ddbfeb393446423c9574
hanhanhanz <69136662+hanhanhanz@users.noreply.github.com>
2021-05-16 16:11:29 +0700

    major update
    add log, -v option, iteration url grepping, goroutine

--- urlgrepper.go
+++ urlgrepper.go
@@ -11,8 +11,11 @@ import "net/url"
 import "strings"
 import "sync"
 import "net/http"
+import "net"
 import "crypto/tls"
 import "syscall"
+import "log"
+
 //import "errors"
 //import "net"
 //import "github.com/gijsbers/go-pcre"
@@ -25,6 +28,7 @@ type conf struct {
 	Thread int
 	Xtension string
 	Toxtract string
+	Verbose bool
 	Outfile *(os.File)
 }
 
@@ -34,8 +38,7 @@ type error interface {
 
 func storehere(d string, f *(os.File) ) { //store result (in string) to file
 	if _, err := f.WriteString(d); err != nil {
-		fmt.Printf("[-]storing function error :  ")
-		panic(err)
+		log.Fatal("[-] storing function error :  ", err)
 	}	
 }
 
@@ -77,8 +80,8 @@ func urltoslice(url string, urls string) []string {
 	} else if url != "" {
 		_, err := regexp.Match(`^https?://`, []byte(url))
 		if err != nil {
-	    	fmt.Printf("[-] regex.match failed  : ")
-	    	panic(err)
+	    	log.Fatal("[-] regex.match failed  : ",err)
+	    	
 	    }
 		seeds = append(seeds,url)
 		
@@ -93,8 +96,8 @@ func urltoslice(url string, urls string) []string {
     		var line = g2.Text()
     		_, err := regexp.Match(`^https?://`, []byte(url))
 			if err != nil {
-	    		fmt.Printf("[-] regex.match failed  : ")
-	    		panic(err)
+	    		log.Fatal("[-] regex.match failed  : ",err)
+	    		
 	    	}
 			seeds = append(seeds,line)
 			
@@ -105,21 +108,48 @@ func urltoslice(url string, urls string) []string {
 }
 
 
-func myrequest(nc *retryablehttp.Client, seed string, wg *sync.WaitGroup)  (error,string) {
+func myrequest(nc *retryablehttp.Client, seed string)  (error,string) {
+	//fmt.Println("doMyRequest")
 	body2 := ""
 
-		req, err := retryablehttp.NewRequest("GET",seed, nil)
+		//fmt.Println(seed)
+		unparse,err := url.QueryUnescape(seed)
+		u,err := url.Parse(unparse)
+		//fmt.Println(u.Host)
+		var domain = ""
+
 		if err != nil {
-	    	wg.Done()
+			log.Println("[-] error, something wrong when parsing the url in directory: %s",err)
+		}
+		
+		if u.Scheme == "" { //parsing when no http schema
+			u.Scheme = "https" 
+
+			domain = u.Scheme + "://" + seed
+		
+		} else { //parsing when there's http schema
+			//domain = u.Scheme + "://" + u.Host + "/"
+			domain = u.Scheme + "://" + u.Host 
+			//temp = strings.Replace(u.Path,"/","",1)
+		}
+
+		req, err := retryablehttp.NewRequest("GET",domain, nil)
+
+		if err != nil {
+	    	//wg.Done()
+	    	log.Println("[-] Generate NewRequest failed : ",err)
 			return err,body2
 	    }
 		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
-
-		//send request
-		resp, err := nc.Do(req)
 		
+		//send request
+		//fmt.Printf(domain + " ")
+		resp, err := nc.Do(req)
+		//resp, err = http.Get(domain)
+		//fmt.Println(resp.StatusCode)
 		if err != nil {
-			wg.Done()
+			//wg.Done()
+			log.Println("[-] request failure : ", err)
 			return err,body2
 		}
 		
@@ -127,12 +157,14 @@ func myrequest(nc *retryablehttp.Client, seed string, wg *sync.WaitGroup)  (erro
 		//reading request
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-	    	wg.Done()
+	    	//wg.Done()
+	    	log.Println("[-] response reading failure : ", err)
 			return err,body2
 	    }
-	   resp.Body.Close()
+
+	  resp.Body.Close()
 	body2 = string(body)
-	wg.Done()
+	//wg.Done()
 	return err,body2
 }
 
@@ -154,8 +186,8 @@ func urlprocess(body2 string, seed string, c conf, wg *sync.WaitGroup) ([]string
 	//migrate to url format for easy URL segmentation
 	unparse,err := url.QueryUnescape(seed)
 	if err != nil {
-		fmt.Printf("[-] QueryUnescape failed  : ")
-		panic(err)
+		log.Fatal("[-] QueryUnescape failed  : ",err)
+		
 	}
 
 	//get domain
@@ -163,15 +195,15 @@ func urlprocess(body2 string, seed string, c conf, wg *sync.WaitGroup) ([]string
 	if c.Toxtract != "" {
 		parseed,err = url.Parse(c.Toxtract)
 		if err != nil {
-			fmt.Printf("[-] get domain url.Parse1 failed  : ")
-			panic(err)
+			log.Fatal("[-] get domain url.Parse1 failed  : ",err)
+			
 		}
 
 	} else {
 		parseed,err = url.Parse(unparse)
 		if err != nil {
-			fmt.Printf("[-] get domain url.Parse2 failed  : ")
-			panic(err)
+			log.Fatal("[-] get domain url.Parse2 failed  : ",err)
+			
 		}
 	}
 	
@@ -182,21 +214,23 @@ func urlprocess(body2 string, seed string, c conf, wg *sync.WaitGroup) ([]string
 			//domain regex
 			unparse,err := url.QueryUnescape(raw)
 			if err != nil {
-    			fmt.Printf("[-] QueryUnescape failed  : ")
-    			panic(err)
+    			log.Fatal("[-] QueryUnescape failed  : ",err)
+    			
     		}
 			
 			u,err := url.Parse(unparse)
 			if err != nil {
-    			fmt.Printf("[-] process url.Parse failed  : ")
-    			panic(err)
+    			log.Println("[-] process url.Parse failed  : ",err)
+    			break
+    			//panic(err)
     		}
 
 			//parse to obtain domain/subdomain specified in -u/-ul
 			
 			tem := strings.ReplaceAll(parseed.Host,".","\\.") 	
 							
-			pat2 := regexp.MustCompile(fmt.Sprintf(`(^|^[^:]+:\/\/|[^\.]+\.)`+tem))
+			//pat2 := regexp.MustCompile(fmt.Sprintf(`(^|^[^:]+:\/\/|[^\.]+\.)`+tem))
+			pat2 := regexp.MustCompile(fmt.Sprintf(`(\*\.)?([\w\d]+\.)+[\w\d]?`+tem))
 			//pat2 := regexp.MustCompile(fmt.Sprintf(`(^|^[^:]+:\/\/|[^\.]+\.)w3\.com`))
 			temp := pat2.FindAllString(u.Host,-1)
 			if temp != nil {
@@ -234,32 +268,182 @@ func urlprocess(body2 string, seed string, c conf, wg *sync.WaitGroup) ([]string
 			
 		}	
 	}
-	wg.Done()
+	//wg.Done()
 	return clean
 		
 }
 
-func cleanandstore(c conf, clean []string, wg *sync.WaitGroup) {
+func cleanandstore(c conf, clean []string, wg *sync.WaitGroup) (out []string) {
+	out = []string{}
 	clean = removeDuplicateValues(clean)
 	for _, s := range clean {
-		fmt.Println(s)
+		//fmt.Println(s)
+		out = append(out,s)
 		if c.Outname != "" {
 			storehere(s+"\n",c.Outfile)
 		}
 	}
 	wg.Done()
+	return out
 }
 
-func cleanandstore2(c conf, clean []string) {
+func cleanandstore2(c conf, clean []string) (out []string) {
+	out = []string{}
 	clean = removeDuplicateValues(clean)
 	for _, s := range clean {
-		fmt.Println(s)
+		//fmt.Println(s)
+		out = append(out,s)
 		if c.Outname != "" {
 			storehere(s+"\n",c.Outfile)
 		}
 	}
+	return out
 }
 
+
+
+func compareslice(master []string, slave []string) (out bool) {
+	p := 0
+	res :=  false
+	//fmt.Println(len(temp))
+
+			for _,j := range master {
+				for _,i := range slave {
+					
+					if j == i {
+						p += 1
+						//fmt.Println(p)
+						if p == len(slave) {
+							res = true
+						}
+					}
+				}
+			}
+		return res
+}
+
+func compareslice2(master []string, slave []string) (out int) {
+	p := 0
+	res :=  0
+	//fmt.Println(len(temp))
+
+			for _,j := range master {
+				for _,i := range slave {
+					
+					if j == i {
+						p += 1
+						//fmt.Println(p)
+						if p == len(slave) {
+							res = p
+						}
+					}
+				}
+			}
+		return res
+}	
+
+
+func play(c conf, seeds []string ,nc *retryablehttp.Client) (out []string) {
+	    //final result slice declaration
+	    clean := []string{}
+
+		//channel declaration
+		body2 := make(chan string, len(seeds))
+		body3 := make(chan string, len(seeds))
+		body4 := make(chan []string, len(seeds))
+		
+		//goroutine guard number
+	    guard1 := make(chan struct{}, c.Thread)
+	    guard2 := make(chan struct{}, c.Thread)
+	    //guard3 := make(chan struct{}, c.Thread)
+
+	    var wg sync.WaitGroup
+	 	//wg.Add(len(seeds))
+	//goroutine1=======================================================================================================================================================================================================================
+		//go func() {
+			for _,seed := range seeds {
+				wg.Add(1)		
+				//building request
+				guard1 <- struct{}{}
+				//go func(seed string)  {
+				go func(seed string)  {
+					defer wg.Done()						
+					//fmt.Println("doplaylv1")
+					
+					//fmt.Println()
+					err,data := myrequest(nc,seed)
+					if err != nil {
+						if strings.Contains(err.Error(), "i/o timeout") {
+			    			data = ""
+			    		} else {
+			    			log.Println("[-] myrequest func error : ",err)
+			    			
+			    			data = ""
+			    		} 	
+					}
+
+					body2 <- data
+					body3 <- seed
+
+					
+					<-guard1
+				}(seed)
+				
+			}
+			
+		//}()
+
+		//go func() {
+			//time.Sleep(4 * time.Second)
+			wg.Wait()
+			close(body2)
+			close(body3)
+
+		//}()
+	//goroutine2=======================================================================================================================================================================================================================
+		
+		
+		//wg.Add(len(seeds))
+		//go func() {
+			for i := 0; i < len(seeds); i++ {
+				wg.Add(1)
+				guard2 <- struct{}{}
+				go func(body2 chan string, body3 chan string) {
+					defer wg.Done()	
+					message1 := <-body2
+					message2 := <-body3
+
+					body4 <- urlprocess(message1,message2,c,&wg)
+					
+
+					
+					<-guard2
+				}(body2,body3)
+
+			}
+			
+			wg.Wait()
+			close(body4)
+		//}()
+		
+		
+
+	//goroutine3(optional)=======================================================================================================================================================================================================================				
+		var master = []string{}
+		//if c.Toxtract != "" {
+			
+			for i := 0; i < len(seeds); i++ {
+		
+				message3 := <- body4
+				for _,mes := range message3 {
+					clean = append(clean,mes)
+				}
+			}
+			master = cleanandstore2(c,clean)
+	
+		return master//HERE
+
+}
 
 
 
@@ -274,18 +458,27 @@ func main() {
 	flag.IntVar(&(c.Thread),"t",0,"goroutine number to be utilized (kinda like thread)")
 	flag.StringVar(&(c.Xtension),"x","","xtension to extract (only work for URL mode)")
 	flag.StringVar(&(c.Toxtract),"tx","","specify extract domain if it is different with source URL requested")
+	flag.BoolVar(&(c.Verbose),"v",false,"enable verbose mode")
 	flag.Parse()
+	
+
+	//switch for verbose mode
+	if c.Verbose == false {
+		log.SetOutput(ioutil.Discard)	
+	}
+	
 
 	///building client
 	//disable cert check
 	customTransport := http.DefaultTransport.(*http.Transport).Clone()
 	customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	customTransport.IdleConnTimeout = time.Second * 5
+	customTransport.IdleConnTimeout = time.Second * 10
+	customTransport.DialContext = (&net.Dialer{Timeout: 10 * time.Second, KeepAlive: 10 * time.Second}).DialContext
 	//default retry conf
 	var nc = retryablehttp.NewClient()
-	nc.RetryWaitMin = 2 * time.Second
-	nc.RetryWaitMax = 2 * time.Second
-	nc.RetryMax = 2
+	nc.RetryWaitMin = 7 * time.Second
+	nc.RetryWaitMax = 10 * time.Second
+	nc.RetryMax = 1
 	nc.Logger = nil
 	nc.HTTPClient = &http.Client{Transport: customTransport}
 	
@@ -295,25 +488,27 @@ func main() {
 
 	//preparing thread
     if c.Thread == 0 {
-		c.Thread = len(seeds) 
+		c.Thread = len(seeds)
+		//fmt.Println(seed)
+		//fmt.Println(c.Thread)
 	}
 
 	//increase file descriptors
 	var rLimit syscall.Rlimit
 	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
 	if err != nil {
-        fmt.Println("Error Getting Rlimit ", err)
+        log.Fatal("[-] Error Getting Rlimit ", err)
     }
     if rLimit.Cur < uint64(c.Thread*3) {
     	rLimit.Cur = uint64(c.Thread*3)
     	err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
     	if err != nil {
-        	fmt.Println("Error Setting Rlimit ", err)
+        	log.Fatal("[-] Error Setting Rlimit ", err)
     	}
     }
     err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
     if err != nil {
-       	fmt.Println("Error Getting Rlimit ", err)
+       	log.Fatal("[-] Error Getting Rlimit ", err)
     }
     //fmt.Println(rLimit)
 
@@ -323,100 +518,104 @@ func main() {
 	    var _ = os.Remove(c.Outname)
 	    f,err := os.OpenFile(c.Outname,os.O_APPEND|os.O_WRONLY|os.O_CREATE,0644)
 	    if err != nil {
-	    	fmt.Printf("[-]create file error : ")
-	    	panic(err)
+	    	log.Fatal("[-] create file error : ",err)
+	    	
 	    }
 	    c.Outfile = f
     }
 	
-	
-    
-    //final result slice declaration
-    clean := []string{}
 
-	//channel declaration
-	body2 := make(chan string, len(seeds))
-	body3 := make(chan string, len(seeds))
-	body4 := make(chan []string, len(seeds))
-	
-	//goroutine guard number
-    guard1 := make(chan struct{}, c.Thread)
-    guard2 := make(chan struct{}, c.Thread)
-    guard3 := make(chan struct{}, c.Thread)
 
-    var wg sync.WaitGroup
- 	wg.Add(len(seeds))
-//goroutine1=======================================================================================================================================================================================================================
-	for _,seed := range seeds {
-		//building request
-		guard1 <- struct{}{}
-		go func(seed string)  {
+
+	//indev============================================================================================================================================================
+	//fmt.Println(seeds)
+
+	//resp, err := nc.Do("https://google.com")
+	//len(resp)
+	time.Sleep(time.Second * 4)
+	var master = []string{}
+	for _,sd := range seeds { 
+		
+		var seed = []string{}
+		seed = append(seed,sd)
+		
+		dodol:=play(c,seed,nc)//HERE
+		
+		
+		/*if len(master) > 1  {
+			c.Thread = len(master) 
+		} else {
+			c.Thread = 1
+		}*/
+
+
+		master = append(master,dodol...)
+		master = removeDuplicateValues(master)
+		fmt.Println(fmt.Sprintf("iteration 0, total domain : %d",len(master)))
+		
+		
+		var new = []string{}
+		
+		c.Toxtract = sd
+		
+		
+		if len(master) >= 1  {
+			c.Thread = len(master) 
+		} else {
+			c.Thread = 1
+		}		
+
+
+		new = append(new,play(c,master,nc)...)	
+		fmt.Println(fmt.Sprintf("iteration 1, total domain : %d",len(removeDuplicateValues(master))))
+
+		t := 0
+		for true {
 			
-			err,data := myrequest(nc,seed,&wg)
-			if err != nil {
-				if strings.Contains(err.Error(), "i/o timeout") {
-	    			data = ""
-	    		} else {
-	    			fmt.Printf("[-] myrequest func error : ")
-	    			fmt.Println(err)
-	    			data = ""
-	    		} 	
+			master = removeDuplicateValues(append(master,new...))
+			if t != 0 {
+				fmt.Println(fmt.Sprintf("iteration %d, total domain : %d",t+1,len(master)))	
+			}
+			
+
+			if (compareslice(master,new) == false || t < 2 ) && (len(new) != 0) { //new value found
+		
+				//sp := fmt.Sprintf("iteration %d with %d domain",t + 2,len(master))
+				//fmt.Println(sp)
+				var new2 = []string{}
+
+				if len(master) > 1  {
+					c.Thread = len(master) - 1
+				} else {
+					c.Thread = 1
+				}
+
+				new2 = append(new2,play(c,new,nc)...) //playnya dibikin goroutine biar gercep?
+				//fmt.Println(new2)
+				//fmt.Println(len(new2))
+				new = new2
+				
+				t += 1
+			
+			} else {
+				//fmt.Println("true")
+				//fmt.Println(t)
+				break
 			}
 
-			body2 <- data
-			body3 <- seed
-		
-			<-guard1
-			
-		}(seed)
-		
-	}
-//goroutine2=======================================================================================================================================================================================================================
-
-	wg.Wait()
-	wg.Add(len(seeds))
-	for i := 0; i < len(seeds); i++ {
-		guard2 <- struct{}{}
-		go func() {
-
-			message1 := <-body2
-			message2 := <-body3
-			body4 <- urlprocess(message1,message2,c,&wg)
-			<-guard2
-		}()
-
-	}
-	
-	wg.Wait()
-	close(body2)
-	close(body3)
-	
-
-//goroutine3(optional)=======================================================================================================================================================================================================================				
-	if c.Toxtract != "" {
-		for i := 0; i < len(seeds); i++ {
-	
-			message3 := <- body4
-			for _,mes := range message3 {
-				clean = append(clean,mes)
-			}
 		}
-		cleanandstore2(c,clean)
-	} else {
-		wg.Add(len(seeds))
 
-		for i := 0; i < len(seeds); i++ {
-			guard3 <- struct{}{}
-			go func() {
-				message3 := <- body4
-				cleanandstore(c,message3,&wg)
-				<-guard3
-			}()
-			
+		master = removeDuplicateValues(master)
+		//sp := fmt.Sprintf("iteration " + string(t) + " with " + string(len(master)) + " domain")
+		//fmt.Println(sp)
+		fmt.Println("final result :")
+		for _,i := range master { 
+			fmt.Println(i)
 		}
-		wg.Wait()
-		close(body4)
-		
+
+		//mesti make https*/
 	}
+
+
 
 }
